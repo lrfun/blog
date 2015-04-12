52c5acbbb9bd978bbb4377ad4888936a5740aa28
lrfun <luorongfang126@126.com>
2015-04-12 13:23:20 +0800

    UI 重构

--- config.js
+++ config.js
@@ -5,14 +5,14 @@ module.exports = {
 	version:'0.0.1',
 	session_secret:'session.lrfun.com',
 	auth_cookie: 'cookies.lrfun.com',
-	db:'mongodb://baimo:baimo@ds049171.mongolab.com:49171/lrfun',//mongodb://127.0.0.1/lrfun
+	db:'mongodb://127.0.0.1/lrfun',//mongodb://127.0.0.1/lrfun
 	host:'localhost',
 	page_size:10,
 	rss:{
 		    title: "lrfun’blog with node.js",
 		    link: 'http://cnodejs.org',
 		    language: 'zh-cn',
-		    description: '这里是芳儿的个人blog（Nodejs版）',
+		    description: 'lrfun@github',
 		    //最多获取的RSS Item数量
 		    max_rss_items: 50
 	},
